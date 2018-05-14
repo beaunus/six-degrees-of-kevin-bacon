580d2a963140ef6a05cff73fec554fe9c04504d6
Beau Dobbin <beau@beaunus.com>
2018-05-14 17:08:10 +0900

    Add simple form for actor input

--- src/NodeSelector.vue
+++ src/NodeSelector.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Node Selector</h1>
+    <label for="actor1">Actor 1</label>
+    <input type="text" id="actor1">
+    <label for="actor1">Actor 2</label>
+    <input type="text" id="actor2">
+    <button name="sumbit-actors">Submit</button>
   </div>
 </template>
 
