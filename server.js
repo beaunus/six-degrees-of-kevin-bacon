480b9ad8f5c0388388a4608a5eda8331c706b8a2
Beau Dobbin <beau@beaunus.com>
2018-05-14 18:10:33 +0900

    API: return request body

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const serveStatic = require("serve-static");
 const path = require("path");
 
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use("/", serveStatic(path.join(__dirname)));
+app.use(bodyParser.json());
+
+app.route("/api").get((req, res) => {
+  res.send(req.body);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(process.env.PORT || 5000);
