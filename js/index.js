48a70641610ce7ad321ce9758c23777e17b6fc8f
Beau Dobbin <beau@beaunus.com>
2018-05-15 17:23:42 +0900

    Move all logic to root index.html and js/index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+async function getPaths(event) {
+  const actor1 = document.getElementById("actor1").value;
+  const actor2 = document.getElementById("actor2").value;
+  const actorsArray = [actor1, actor2];
+  const queryString = actorsArray
+    .map(actorName => `actor=${encodeURI(actorName)}`)
+    .join("&");
+  const paths = await axios.get(`/api?${queryString}`);
+  console.log(paths.data);
+}
