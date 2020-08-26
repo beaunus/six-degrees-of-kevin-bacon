2dbecb227d37660d34a8dcfdd19793e82ca01631
Beau Dobbin <beau@beaunus.com>
2020-08-26 22:23:24 +0900

    feature: use menu for input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import { menuController } from "@ionic/core";
 import {
+  IonApp,
   IonButton,
   IonContent,
   IonHeader,
@@ -7,7 +9,9 @@ import {
   IonItem,
   IonLabel,
   IonList,
+  IonMenu,
   IonPage,
+  IonText,
   IonToolbar,
 } from "@ionic/react";
 import React, { useState } from "react";
@@ -29,61 +33,78 @@ const Home: React.FC = () => {
   const [maxCastPosition, setMaxCastPosition] = useState(10);
 
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>Six Degrees of Kevin Bacon</IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonList>
-          <IonItem>
-            <IonLabel position="floating">Actor 1</IonLabel>
-            <IonInput
-              clearInput
-              onIonChange={(e) =>
-                setActorNames([e.detail.value || "", ...actorNames.slice(1)])
-              }
-              value={actorNames[0]}
-            />
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Actor 2</IonLabel>
-            <IonInput
-              clearInput
-              onIonChange={(e) =>
-                setActorNames([...actorNames.slice(0, 1), e.detail.value || ""])
-              }
-              value={actorNames[1]}
-            />
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Minimum Movie Popularity</IonLabel>
-            <IonInput
-              max="30"
-              min="0"
-              onIonChange={(e) => setMinMoviePopularity(Number(e.detail.value))}
-              type="number"
-              value={minMoviePopularity}
-            />
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Maximum Cast Position</IonLabel>
-            <IonInput
-              min="0"
-              onIonChange={(e) => setMaxCastPosition(Number(e.detail.value))}
-              type="number"
-              value={maxCastPosition}
-            />
-          </IonItem>
-        </IonList>
-        <IonButton expand="full" onClick={handleGoClick}>
-          GO
+    <IonApp>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar class="ion-text-center" color="primary">
+            <IonText>Six Degrees of Kevin Bacon</IonText>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonList>
+            <IonItem>
+              <IonLabel position="floating">Actor 1</IonLabel>
+              <IonInput
+                clearInput
+                onIonChange={(e) =>
+                  setActorNames([e.detail.value || "", ...actorNames.slice(1)])
+                }
+                value={actorNames[0]}
+              />
+            </IonItem>
+            <IonItem>
+              <IonLabel position="floating">Actor 2</IonLabel>
+              <IonInput
+                clearInput
+                onIonChange={(e) =>
+                  setActorNames([
+                    ...actorNames.slice(0, 1),
+                    e.detail.value || "",
+                  ])
+                }
+                value={actorNames[1]}
+              />
+            </IonItem>
+            <IonItem>
+              <IonLabel position="floating">Minimum Movie Popularity</IonLabel>
+              <IonInput
+                max="30"
+                min="0"
+                onIonChange={(e) =>
+                  setMinMoviePopularity(Number(e.detail.value))
+                }
+                type="number"
+                value={minMoviePopularity}
+              />
+            </IonItem>
+            <IonItem>
+              <IonLabel position="floating">Maximum Cast Position</IonLabel>
+              <IonInput
+                min="0"
+                onIonChange={(e) => setMaxCastPosition(Number(e.detail.value))}
+                type="number"
+                value={maxCastPosition}
+              />
+            </IonItem>
+          </IonList>
+          <IonButton expand="full" onClick={handleGoClick}>
+            GO
+          </IonButton>
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonButton expand="block" onClick={() => menuController.open()}>
+          Open Menu
         </IonButton>
-        <Graph {...graph} />
-      </IonContent>
-    </IonPage>
+        <IonContent>
+          <Graph {...graph} />
+        </IonContent>
+      </IonPage>
+    </IonApp>
   );
 
   async function handleGoClick() {
+    menuController.close();
     setGraph(defaultGraph);
     const persons = await getPersons(actorNames);
     const moviesByActorName = Object.fromEntries(
@@ -105,6 +126,6 @@ const Home: React.FC = () => {
   }
 };
 
-setTimeout(() => document.querySelector("ion-button")?.click(), 500);
+setTimeout(() => document.querySelector("IonButton")?.click(), 500);
 
 export default Home;
