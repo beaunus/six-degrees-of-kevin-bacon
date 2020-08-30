55c289d1c01b7332af1c81c45e6e41f81acac01b
Beau Dobbin <beau@beaunus.com>
2020-08-30 16:00:13 +0900

    feature: change default actors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,7 +36,7 @@ const defaultGraph = { links: [], nodes: [] } as {
 };
 
 const Home: React.FC = () => {
-  const [actorNames, setActorNames] = useState(["Bruce Lee", "Tom Hiddleston"]);
+  const [actorNames, setActorNames] = useState(["Kevin Bacon", "Ian McKellen"]);
   const [graph, setGraph] = useState(defaultGraph);
   const [minMoviePopularity, setMinMoviePopularity] = useState(20);
   const [maxCastPosition, setMaxCastPosition] = useState(5);
